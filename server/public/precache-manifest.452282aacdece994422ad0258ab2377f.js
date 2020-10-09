5103de9e9d9d95dc29bd4e3c37f44cedf49b8216
VeXuZ <vexuzdorotan@gmail.com>
2020-10-09 23:52:24 +0800

    Heroku Init

---
+++ server/public/precache-manifest.452282aacdece994422ad0258ab2377f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78c9e013349deafe6a92675486137e53",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df288bfbe8f2bfbaeff8",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "cc395f4f187179ba0118",
+    "url": "/static/css/main.78a56f7f.chunk.css"
+  },
+  {
+    "revision": "df288bfbe8f2bfbaeff8",
+    "url": "/static/js/2.ee4ffd70.chunk.js"
+  },
+  {
+    "revision": "1690ebe2c16e6acb393967391e66d0b1",
+    "url": "/static/js/2.ee4ffd70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc395f4f187179ba0118",
+    "url": "/static/js/main.a799c807.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,6 @@
 require('dotenv').config({ path: './config/config.env' });
 require('./db/mongoose')();
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
@@ -10,6 +11,9 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
+
+app.use(express.static(path.join('public')));
+
 app.use(cors());
 
 if (process.env.NODE_ENV === 'development') {
@@ -17,6 +21,10 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 app.use('/favorites', favoriteRouters);
+
+app.use((req, res, next) => {
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+});
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}.`);
